4d71e8101035d2ab68ccbac534f2e0f92474af6b
Ankitha0406 <kulalankitha2@gmail.com>
2026-09-17 21:16:53 +0530

    random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -8,7 +8,7 @@ def run_random_forest():
     X_test = load_npz("data/X_test.npz")
     y_train = np.load("data/y_train.npy")
     y_test = np.load("data/y_test.npy")
-    print("Training Random Forest Classifier...")
+    print("Training Random Forest Classifier....")
     model = RandomForestClassifier(n_estimators=100, max_depth=20, n_jobs=-1, random_state=42)
     model.fit(X_train, y_train)
     print("Evaluating model...")
